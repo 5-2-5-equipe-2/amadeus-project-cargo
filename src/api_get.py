08b9c0096c9f278078a712644f0f6742cd08007c
Apoorva64 <appadooapoorva@gmail.com>
2022-10-14 22:53:17 +0200

    wdcf23dwqdqc

--- src/api_get.py
+++ src/api_get.py
@@ -8,6 +8,22 @@ import requests
 # POST https://af-cargo-api-cargo.azuremicroservices.io/api/submit to submit your answer with an object containing the 5 compartment with their containers and shipments (shipment id list).
 #
 
+class Shipment:
+    """Rectangle Shipment class."""
+
+    def __init__(self, weight: float, height, width, length, awb):
+        self.shipment_id = awb
+        self.weight = weight
+        self.awb = awb
+        self.width = width
+        self.height = height
+        self.length = length
+        self.volume = self.width * self.height * self.length
+        self.density = self.weight / self.volume
+
+    def __str__(self):
+        return f'Shipment({self.shipment_id}, {self.weight}, {self.width}, {self.height}, {self.length}, {self.volume}, {self.density})'
+
 
 def get_compartments():
     """Get all compartments from API."""
@@ -15,16 +31,29 @@ def get_compartments():
     return response.json()
 
 
+class ContainerType:
+    def __init__(self, container_type, height, width, length):
+        self.container_type = container_type
+        self.height = height
+        self.width = width
+        self.length = length
+
+    def __str__(self):
+        return f'ContainerType({self.container_type}, {self.height}, {self.width}, {self.length})'
+
+
 def get_container_types():
     """Get all container types from API."""
     response = requests.get("https://af-cargo-api-cargo.azuremicroservices.io/api/container")
-    return response.json()
+    return [ContainerType(container_type["type"], container_type["height"], container_type["width"],
+                          container_type["length"]) for container_type in response.json()]
 
 
 def get_shipments():
     """Get all shipments from API."""
     response = requests.get("https://af-cargo-api-cargo.azuremicroservices.io/api/shipment")
-    return response.json()
+    return [Shipment(shipment["weight"], shipment["height"], shipment["width"], shipment["length"], shipment["awb"]) for
+            shipment in response.json()]
 
 
 def get_luggage():
--- src/shipments.py
+++ src/shipments.py
@@ -1,23 +1,37 @@
-from api_get import get_container_types
+from typing import Dict, List
+
+from api_get import get_container_types, ContainerType, get_shipments, Shipment
 
 
-class Shipment:
-    """Rectangle Shipment class."""
+def sort_shipments(shipments: [Shipment], container_types: [ContainerType]):
+    """Sort shipments by size."""
+    shipments_dict = {}
 
-    def __init__(self, weight: float, height, width, length, awb):
-        self.shipment_id = awb
-        self.weight = weight
-        self.awb = awb
-        self.volume = height * width * length
+    # sort container types by volume
+    container_types.sort(key=lambda x: x.height * x.width * x.length)
 
-    def __str__(self):
-        return f'Shipment( {self.weight}, {self.volume}, {self.awb})'
+    for shipment in shipments:
+        for container_type in container_types:
+            if shipment.width <= container_type.width and shipment.height <= container_type.height \
+                    and shipment.length <= container_type.length:
+                if container_type.container_type in shipments_dict:
+                    shipments_dict[container_type.container_type].append(shipment)
+                else:
+                    shipments_dict[container_type.container_type] = [shipment]
+                break
+
+    for container_type in shipments_dict:
+        shipments_dict[container_type].sort(key=lambda _shipment: _shipment.density, reverse=True)
+
+    return shipments_dict
 
 
-
-
-def sort_shipments(shipments):
-    """Sort shipments by weight."""
+if __name__ == '__main__':
+    # Get all shipments
+    shipments = get_shipments()
+    # Get all container types
     container_types = get_container_types()
-
-
+    # Sort shipments by size
+    sorted_shipments = sort_shipments(shipments, container_types)
+    # Print sorted shipments
+    pass
